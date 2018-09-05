75f14facaca9c34242803a6b563387e854e7a337
Alexander Gough <gougha3@my.erau.edu>
2018-09-05 07:42:36 -0400

    Add project files.

---
+++ Lab_5B_Gough/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_5B_Gough
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
